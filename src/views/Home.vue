7554fcdb068ad8e128d84b63b1a67d86c6d3d446
MarcBad98 <badi_mar@outlook.com>
2021-03-27 21:53:40 -0400

    added vote system to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,33 +3,83 @@
     <DemoWarning />
     <h1 class="title">Welcome to Whistlechain</h1>
     <hr />
-    <div v-for="whistleblow in whistleblows" v-bind:key="whistleblow.id">
-      <GenericCard :title="whistleblow.title">
-        <b-taglist>
-          <b-tag
-            v-for="tag in whistleblow.tags"
-            v-bind:key="tag"
-            type="is-info"
-            rounded
-          >
-            {{ tag }}
-          </b-tag>
-        </b-taglist>
-        <p>{{ whistleblow.description }}</p>
-        <b-button
-          tag="router-link"
-          :to="{
-            name: 'ViewWhistleblow',
-            params: { id: whistleblow.id },
-          }"
-          label="View Whistleblow Form"
-          type="is-light"
-          size="is-small"
-          icon-pack="fas"
-          icon-left="eye"
-        ></b-button>
-      </GenericCard>
-      <br />
+    <div class="columns is-multiline">
+      <div
+        v-for="whistleblow in whistleblows"
+        v-bind:key="whistleblow.id"
+        class="column is-half"
+      >
+        <GenericCard :title="whistleblow.title" style="height: 100%">
+          <b-taglist>
+            <b-tag
+              v-for="tag in whistleblow.tags"
+              v-bind:key="tag"
+              type="is-info"
+              rounded
+            >
+              {{ tag }}
+            </b-tag>
+          </b-taglist>
+          <p>{{ whistleblow.description }}</p>
+          <div class="buttons">
+            <b-button
+              tag="router-link"
+              :to="{
+                name: 'ViewWhistleblow',
+                params: { id: whistleblow.id },
+              }"
+              label="View Whistleblow Form"
+              type="is-light"
+              size="is-small"
+              icon-pack="fas"
+              icon-left="eye"
+            ></b-button>
+            <b-field grouped>
+              <b-field>
+                <b-select size="is-small">
+                  <option
+                    v-for="option in voteOptions"
+                    v-bind:key="option.name"
+                    :value="option"
+                  >
+                    {{ option.name }} ({{ option.votesUp }})
+                  </option>
+                </b-select>
+                <p class="control">
+                  <b-button
+                    type="is-primary"
+                    size="is-small"
+                    icon-pack="fas"
+                    icon-right="thumbs-up"
+                    @click="vote(true)"
+                  ></b-button>
+                </p>
+              </b-field>
+              <b-field>
+                <b-select size="is-small">
+                  <option
+                    v-for="option in voteOptions"
+                    v-bind:key="option.name"
+                    :value="option"
+                  >
+                    {{ option.name }} ({{ option.votesDown }})
+                  </option>
+                </b-select>
+                <p class="control">
+                  <b-button
+                    type="is-primary"
+                    size="is-small"
+                    icon-pack="fas"
+                    icon-right="thumbs-down"
+                    @click="vote(false)"
+                  ></b-button>
+                </p>
+              </b-field>
+            </b-field>
+          </div>
+        </GenericCard>
+        <br />
+      </div>
     </div>
   </div>
 </template>
@@ -45,9 +95,25 @@ export default {
     DemoWarning,
     GenericCard,
   },
+  data() {
+    return {
+      voteOptions: [
+        { name: "Quality", votesUp: 258, votesDown: 884 },
+        { name: "Accurate", votesUp: 40, votesDown: 826 },
+        { name: "Professional", votesUp: 923, votesDown: 649 },
+        { name: "Relevant", votesUp: 28, votesDown: 499 },
+        { name: "Insightful", votesUp: 621, votesDown: 375 },
+      ],
+    };
+  },
   computed: {
     whistleblows() {
       return MockWhistleblows;
+    },
+  },
+  methods: {
+    vote(isUpVote) {
+      this.$buefy.snackbar.open(`${isUpVote ? "Upvoted" : "Downvoted"}`);
     },
   },
 };
